0d6c97d9fc3f25693626bc88d28954c0e6d3916b
thiaguinhodelas <145620188+thiaguinhodelas@users.noreply.github.com>
2023-10-25 20:18:15 -0400

    guit alves

---
+++ main.js
@@ -0,0 +1,14 @@
+const num1 = document.getElementsByClassName('num1');
+const num2 = document.getElementsByClassName('num2');  
+const button = document.getElementsByClassName('calculaar');
+
+combineBtn.addEventListener("click", () => {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+
+    return num1 + num2
+});
+
+function calculadoraa(num1, str2) {
+    return combineBtn
+}
